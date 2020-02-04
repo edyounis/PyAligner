21ba3e42494014c48ff37a3fb1549dbdbc02879b
Ed Younis <edyounis123@gmail.com>
2020-02-04 12:15:18 -0800

    Created DPMatrix

--- sw_xdrop_py/score.py
+++ sw_xdrop_py/score.py
@@ -1,22 +1,36 @@
 class Scorer():
-    """Scorer Class"""
+	"""Scorer Class"""
 
-    def __init__ ( self, match, mismatch, gap, affine = None ):
+	def __init__ ( self, match, mismatch, gap, affine = None ):
 
-        if not isinstance( match, int ):
-            raise TypeError( "Invalid Match Score Type" )
+		if not isinstance( match, int ):
+			raise TypeError( "Invalid Match Score Type" )
 
-        if not isinstance( mismatch, int ):
-            raise TypeError( "Invalid Mismatch Score Type" )
+		if not isinstance( mismatch, int ):
+			raise TypeError( "Invalid Mismatch Score Type" )
 
-        if not isinstance( gap, int ):
-            raise TypeError( "Invalid Gap Score Type" )
+		if not isinstance( gap, int ):
+			raise TypeError( "Invalid Gap Score Type" )
 
-        if affine is not None and not isinstance( affine, int ):
-            raise TypeError( "Invalid Affine Gap Score Type" )
+		if affine is not None and not isinstance( affine, int ):
+			raise TypeError( "Invalid Affine Gap Score Type" )
 
-        self.match    = match
-        self.mismatch = mismatch
-        self.gap      = gap
-        self.affine   = affine
+		self.match    = match
+		self.mismatch = mismatch
+		self.gap      = gap
+		self.affine   = affine
 
+	def calc_square_value ( self, left_score, above_score, diag_score,
+							nuch, nucv ):
+
+		potential_values = []
+
+		if nuch == nucv:
+			potential_values.append( diag_score + self.match )
+		else:
+			potential_values.append( diag_score + self.mismatch )
+
+		potential_values.append( left_score + self.gap )
+		potential_values.append( above_score + self.gap )
+
+		return max( potential_values )
--- sw_xdrop_py/sw.py
+++ sw_xdrop_py/sw.py
@@ -1,41 +1,49 @@
+# Todo:
+# Make a proper python package
+# Add __str__ __repr__ funcs to objects
+# Add Documentation + Proper comments
+
 import os
 import argparse
 from seq import Sequence
 from score import Scorer
+from matrix import DPMatrix
 
-def smith_waterman ( seq1, seq2, scorer ):
-	h_dim = len( seq1 ) + 1
-	v_dim = len( seq2 ) + 1
+# def smith_waterman ( seq1, seq2, scorer ):
+# 	h_dim = len( seq1 )
+# 	v_dim = len( seq2 )
 
-	dp_matrix = [ [ 0 for v in range( v_dim ) ] for h in range( h_dim ) ]
+#     dp_matrix = DPMatrix( seq1, seq2 )
 
-	def print_dp_matrix( dp_matrix ):
-		for r in dp_matrix:
-			print( r )
+# 	dp_matrix = [ [ 0 for v in range( v_dim ) ] for h in range( h_dim ) ]
 
-	for h in range( 1, h_dim ):
-		dp_matrix[h][0] = -h
+# 	def print_dp_matrix( dp_matrix ):
+# 		for r in dp_matrix:
+# 			print( r )
 
-	for v in range( 1, v_dim ):
-		dp_matrix[0][v] = -v
+# 	for h in range( 1, h_dim ):
+# 		dp_matrix[h][0] = -h
 
-	max_value = -1000000
+# 	for v in range( 1, v_dim ):
+# 		dp_matrix[0][v] = -v
 
-	for r, h in enumerate( seq1 ):
-	    for c, v in enumerate( seq2 ):
-	        r_p = r + 1
-	        c_p = c + 1
-	        onef  = dp_matrix[r_p-1][c_p-1]
-	        onef += scorer.match if h == v else scorer.mismatch
+# 	max_value = -1000000
 
-	        twof = max( dp_matrix[r_p-1][c_p], dp_matrix[r_p][c_p-1] ) + scorer.gap
+# 	for r, h in enumerate( seq1 ):
+# 	    for c, v in enumerate( seq2 ):
+# 	        r_p = r + 1
+# 	        c_p = c + 1
+# 	        onef  = dp_matrix[r_p-1][c_p-1]
+# 	        onef += scorer.match if h == v else scorer.mismatch
 
-	        dp_matrix[r_p][c_p] = max( twof, onef )
-	        if ( dp_matrix[r_p][c_p] > max_value ):
-	            max_value = dp_matrix[r_p][c_p]
+# 	        twof = max( dp_matrix[r_p-1][c_p], dp_matrix[r_p][c_p-1] ) + scorer.gap
 
-	print_dp_matrix( dp_matrix )
-	print( max_value )
+# 	        dp_matrix[r_p][c_p] = max( twof, onef )
+# 	        if ( dp_matrix[r_p][c_p] > max_value ):
+# 	            max_value = dp_matrix[r_p][c_p]
+
+# 	print_dp_matrix( dp_matrix )
+# 	print( max_value )
 
 if __name__ == "__main__":
 	description = "Smith-Waterman Algorithm with X-Drop"
@@ -70,6 +78,7 @@ if __name__ == "__main__":
 	scorer = Scorer( args.match_score, args.mismatch_score,
 					 args.gap_score, args.affine_score )
 
-	dp_matrix = smith_waterman( seq1, seq2, scorer )
+	dp_matrix = DPMatrix( seq1, seq2, scorer )
+	print( dp_matrix )
 
 
